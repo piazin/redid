13c8219abb42ab395dfb88e2b90db8667a92b4a1
Lucas <ls4803326@gmail.com>
2022-07-19 17:23:32 -0300

    remove message select 1 + 1

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -6,7 +6,8 @@ const {name, user, password, host, dialect} = config.database;
 const dbconnection = new Sequelize(name, user, password, {
     host: host,
     dialect: dialect,
-    timezone: '-03:00'
+    timezone: '-03:00',
+    logging: false
 });
 
 module.exports = dbconnection;
