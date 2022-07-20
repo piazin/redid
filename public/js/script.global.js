763165a67b6d1fda85c58dac033a4db1d57276b8
Lucas <ls4803326@gmail.com>
2022-07-20 17:44:49 -0300

    adc codesample editor

--- public/js/script.global.js
+++ public/js/script.global.js
@@ -6,13 +6,25 @@ tinymce.init({
     plugins: [
         'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
         'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
-        'media', 'table', 'emoticons', 'template', 'help', 'autosave'
+        'media', 'table', 'emoticons', 'template', 'help', 'autosave', 'codesample'
+    ],
+    codesample_languages: [
+        {text: 'HTML/XML', value: 'markup'},
+        {text: 'JavaScript', value: 'javascript'},
+        {text: 'CSS', value: 'css'},
+        {text: 'PHP', value: 'php'},
+        {text: 'Ruby', value: 'ruby'},
+        {text: 'Python', value: 'python'},
+        {text: 'Java', value: 'java'},
+        {text: 'C', value: 'c'},
+        {text: 'C#', value: 'csharp'},
+        {text: 'C++', value: 'cpp'}
     ],
     toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
     'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
-    'forecolor backcolor emoticons | help',
+    'forecolor backcolor emoticons | help  ' ,
     menu: {
-      favs: { title: 'Meu favoritos', items: ' fullscreen | emoticons | code visualaid | searchreplace ' }
+      favs: { title: 'Meu favoritos', items: ' fullscreen | emoticons | code visualaid | searchreplace | codesample ' }
     },
     menubar: 'favs file edit view insert format tools table help',
     language: 'pt_BR',
