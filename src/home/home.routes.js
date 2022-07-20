35ef9e9b85ffe4e4b5a6d6d42dd8fff29033de5a
piazin <ls4803326@gmail.com>
2022-07-20 01:11:45 -0300

    fixed bug router

--- src/home/home.routes.js
+++ src/home/home.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const HomeControllers = require('./homeControllers');
 
 router.get('/', HomeControllers.render_home);
-router.get('/:slug', HomeControllers.render_document);
+router.get('/document/:slug', HomeControllers.render_document);
 router.get('/category/:slug', HomeControllers.render_category_by_slug);
 
 module.exports = router;
