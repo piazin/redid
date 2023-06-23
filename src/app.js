e8ba1553fe96a5976467c3ed3a1a3ee1e9039e04
Lucas Souza <lucas@pop-os.localdomain>
2023-06-23 14:15:51 -0300

    :bug: fix: redis connect

--- src/app.js
+++ src/app.js
@@ -16,9 +16,9 @@ const documentRouter = require('./document/document.routes');
 const RedisStore = connectRedis(session);
 
 const redisClient = redis.createClient({
-  host: 'red-ci8qshlgkuvmfns51v10',
+  host: config.redis_host,
   port: 6379,
-
+  password: '',
   legacyMode: true,
 });
 
